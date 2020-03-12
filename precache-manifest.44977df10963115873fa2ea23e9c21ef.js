367c9d14b583bbce1a9574fa4cc3e47b75587688
Yurii Holiuk <bionicuss@gmail.com>
2020-03-12 17:39:52 +0200

    Updates

---
+++ precache-manifest.44977df10963115873fa2ea23e9c21ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987d796123a7aa234bd2242610ce877e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5cd4926e2b36d6d95ec0",
+    "url": "./static/js/2.3cfa5c25.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.3cfa5c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8f41f1103d4f58683bc",
+    "url": "./static/js/main.a32c800c.chunk.js"
+  },
+  {
+    "revision": "85cec487fb57dc32e2bb",
+    "url": "./static/js/runtime-main.545869fc.js"
+  }
+]);
